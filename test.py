7b48f1efc61fa5e786f5e8b83de7d49b23611af0
Hermant Julien <jhermant001@o7480-09.math.u-bordeaux1.fr>
2026-01-26 12:50:58 +0100

    blabla

--- test.py
+++
@@ -1,4 +0,0 @@
-from torchvision.datasets import CIFAR10
-import torchvision.transforms as transforms
-
-trainset = CIFAR10(root="./data", train = True, download=True,transform = transforms.ToTensor())
